ee70bb822fb1044fff7055d73c0518481c2f7105
siddharth dwivedi <siddharthdwivedi318@gmail.com>
2022-06-26 23:59:31 +0530

    Add project files.

---
+++ Earbuds_Webservice/Controllers/EarBudsController.cs
@@ -0,0 +1,21 @@
+﻿using Earbuds_Webservice.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Earbuds_Webservice.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EarBudsController : Controller
+    {
+        public List<Earbud> GetEarbuds()
+        {
+            List<Earbud> lst = new List<Earbud>();
+            lst.Add(new Earbud() { EarbudID = 1, EarbudName = "MaxPro", Cost = 2000 });
+            lst.Add(new Earbud() { EarbudID = 2, EarbudName = "Air2", Cost = 4000 });
+            lst.Add(new Earbud() { EarbudID = 3, EarbudName = "Dopes", Cost = 6000 });
+            return lst;
+
+        }
+    }
+}
---
+++ Earbuds_Webservice/Model/Earbud.cs
@@ -0,0 +1,9 @@
+﻿namespace Earbuds_Webservice.Model
+{
+    public class Earbud
+    {
+        public int EarbudID { get; set; }
+        public string EarbudName { get; set; }
+        public int Cost { get; set; }
+    }
+}
